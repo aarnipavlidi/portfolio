edbdcea7b4b8365dbd27704bb6096db1beccdaed
Aarni Pavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2023-11-26 18:05:17 +0200

    Deleting one file (not needed anymore)

--- components/ContentBlock/index.tsx
+++ components/ContentBlock/index.tsx
@@ -18,8 +18,6 @@ interface ContentBlockProps {
   images?: ImageGalleryFragment | null;
 };
 
-// TODO DELETE LATER
-// SMALL TEST
 const ContentBlock: React.FC<ContentBlockProps> = (props) => {
 
   const validateContentBlockTitle = props.title && props.title
